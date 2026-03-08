5b47a048b3e2831911d7b545a71e9fc9ccda5544
preethikasubramani <preethikasubramani02@gmail.com>
2026-03-08 09:53:45 +0000

    Added index.js to run the server

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+});
